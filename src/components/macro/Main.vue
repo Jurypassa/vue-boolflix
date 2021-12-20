fd58719d53816effd0a4abac69d11b326f2310d6
Jury <jurypassa@gmail.com>
2021-12-20 19:02:30 +0100

    Milestone 1

---
+++ src/components/macro/Main.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+      <input v-model="Search" type="text" placeholder="Inserisci il titolo">
+      <button @click="ListaFilm">Cerca</button>
+      <div v-for="(card, index) in TitoliFiltrati" :key="index">
+        <CardFilm :info="card"/>
+      </div>
+  </div>
+</template>
+
+<script>
+import CardFilm from "../section/CardFilm.vue";
+import axios from "axios";
+
+export default {
+  name: "Main",
+  components:{
+    CardFilm,
+  },
+  data(){
+    return{
+      Titoli: null,
+      Search: ""
+    }
+  },
+  methods: {
+    ListaFilm(){
+      axios.get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+          api_key: "073691ba15bec70d857f6e60347f02c7",
+          query: "ritorno"
+        }
+      })
+      .then((response) => {
+        this.Titoli = response.data.results;
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+    },
+    RicercaTitoli(payload){
+      this.Search = payload;
+    },
+  },
+  computed: {
+    TitoliFiltrati(){
+      return this.Titoli.filter((elm) => {
+          return elm.title.includes(this.Search);
+      })
+    }
+  }
+}  
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
---
+++ src/components/section/CardFilm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <p>{{info.title}}</p>
+      <p>{{info.original_title}}</p>
+      <p>{{info.original_language}}</p>
+      <p>{{info.vote_average}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "CardFilm",
+    props:{
+        info: Object
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
