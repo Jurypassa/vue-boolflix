21eb33746f6031bc126296930c274162866e22c0
Jury <jurypassa@gmail.com>
2021-12-24 15:02:58 +0100

    Milestone 3

--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -2,13 +2,20 @@
   <div>
       <input v-model="Search" type="text" placeholder="Inserisci il titolo">
       <button @click="ListaFilm">Cerca</button>
-      <div v-for="(card, index) in TitoliFiltrati" :key="index">
+      <h2>Lista Film</h2>
+      <div v-for="(card, index) in TitoliFilm" :key="index">
+        <CardFilm :info="card"/>
+      </div>
+      <h2>Lista Serie TV</h2>
+      <div v-for="(card, index) in TitoliSerie" :key="index">
         <CardFilm :info="card"/>
       </div>
   </div>
 </template>
 
 <script>
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 import CardFilm from "../section/CardFilm.vue";
 import axios from "axios";
 
@@ -19,36 +26,49 @@ export default {
   },
   data(){
     return{
-      Titoli: [],
-      Search: ""
+      TitoliFilm: [],
+      TitoliSerie: [],
+      Search: "",
+      oldSearch: ""
     }
   },
   methods: {
     ListaFilm(){
-      axios.get('https://api.themoviedb.org/3/search/movie', {
-        params: {
-          api_key: "073691ba15bec70d857f6e60347f02c7",
-          query: this.Search
-        }
-      })
-      .then((response) => {
-        this.Titoli = response.data.results;
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
+      if( this.Search != this.oldSearch ) {
+        axios.get('https://api.themoviedb.org/3/search/movie/', {
+          params: {
+            api_key: "073691ba15bec70d857f6e60347f02c7",
+            query: this.Search
+          }
+        })
+        .then((response) => {
+          this.TitoliFilm = response.data.results;
+          this.oldSearch = this.Search;
+        })
+        .catch(function (error) {
+          console.log(error);
+        }),
+
+        axios.get('https://api.themoviedb.org/3/search/tv/', {
+          params: {
+            api_key: "073691ba15bec70d857f6e60347f02c7",
+            query: this.Search
+          }
+        })
+        .then((response) => {
+          this.TitoliSerie = response.data.results;
+          this.oldSearch = this.Search;
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+      }
     },
     RicercaTitoli(payload){
       this.Search = payload;
     },
   },
-  computed: {
-    TitoliFiltrati(){
-      return this.Titoli.filter((elm) => {
-          return elm.title.includes(this.Search);
-      })
-    }
-  }
+  
 }  
 </script>
 
--- src/components/section/CardFilm.vue
+++ src/components/section/CardFilm.vue
@@ -1,10 +1,55 @@
 <template>
-  <div>
-      <p>{{info.title}}</p>
-      <p>{{info.original_title}}</p>
-      <p>{{info.original_language}}</p>
-      <p>{{info.vote_average}}</p>
-  </div>
+  <section>
+      <p>{{info.title || info.name}}</p>
+      <p>{{info.original_title || info.original_name}}</p>
+      <img v-if="info.backdrop_path" :src="`https://image.tmdb.org/t/p/w342/${info.backdrop_path}`" alt="img">
+      <img v-else src="../../assets/img/riciak.jpg" alt="img" width="342px">
+      <img v-if="flags.includes(info.original_language)" :src="require(`../../assets/img/${info.original_language}.png`)" :alt="info.original_language">
+      <img v-else src="../../assets/img/eu.png" alt="img">
+      <div class="star" v-show="stelle == 0">
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <div class="star" v-show="stelle == 1">
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <div class="star" v-show="stelle == 2">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <div class="star" v-show="stelle == 3">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <div class="star" v-show="stelle == 4">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+      </div>
+      <div class="star" v-show="stelle == 5">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+      </div>
+      
+  </section>
 </template>
 
 <script>
@@ -12,10 +57,32 @@ export default {
     name: "CardFilm",
     props:{
         info: Object
-    }
+    },
+    data(){
+        return{
+            flags: [
+                "it",
+                "fr",
+                "ja",
+                "en",
+                "es"
+            ],
+        }
+    },
+    computed: {
+        stelle(){
+          return Math.ceil(this.info.vote_average / 2);
+        }
+    },
 }
 </script>
 
 <style lang="scss" scoped>
+    .star{
+        color: rgba(216, 216, 6, 0.781);
+    }
 
+    img{
+        display: block;
+    }
 </style>
